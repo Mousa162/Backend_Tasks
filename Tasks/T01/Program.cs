80702cd164ea5621ad5abf156a0f2b910c2c128b
Mousa162 <mohamed.abdelhamid.ebrahim@gmail.com>
2025-03-02 13:26:30 +0200

    Add files via upload

---
+++ Tasks/T01/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace T01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int small = 25;
+            int big = 35;
+
+            double tax = 0.06;
+
+            Console.Write("Enter Number of small carpets: ");
+            int smallCarpets = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter Number of big carpets: ");
+            int bigCarpets = Convert.ToInt32(Console.ReadLine());
+
+            double total = (small * smallCarpets) + (big * bigCarpets);
+
+            double totalTax = total * tax;
+
+            double totalCost = total + totalTax;
+
+
+            Console.WriteLine("Total Cost: " + totalCost);
+
+        }
+    }
+}
